085d41d3bfbe065bfa3ff8445ee5528c07c2c104
Marouane Boujlal <eboujlal@gmail.com>
2019-12-06 16:09:07 +0100

    Update example.py

--- example.py
+++ example.py
@@ -8,5 +8,5 @@ class NumbersThreading(BaseThreading):
 
 
 nt = NumbersThreading(
-    raw_list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], number_of_threads=28)
+    raw_list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], number_of_threads=28,options={})
 nt.start()
